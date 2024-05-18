2379b636213e7c77c5a5e88f77b6644c2ab52fec
ZeeshanDEVPRO <professionalid2003@gmail.com>
2024-05-18 15:14:47 +0530

    updated

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,6 +1,8 @@
 //user landing page for articles and notices
 import { useEffect,useState } from "react"
 import {useNavigate} from "react-router-dom"
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 const HomePage = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -12,7 +14,10 @@ const HomePage = () => {
   };
 
   return (
-    <div>HomePagehnn</div>
+    <div>
+      <Nav/>
+      <Footer/>
+    </div>
   )
 }
 
