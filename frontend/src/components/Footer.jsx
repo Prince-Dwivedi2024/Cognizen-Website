baff65f516ae9b936d9f8e4f97eed47a91c2189f
ZeeshanDEVPRO <professionalid2003@gmail.com>
2024-05-20 10:54:30 +0530

    footer fixed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,9 +8,8 @@ import CognizenLogo2 from '../assets/CognizenLogo2.png'
 
 const Footer = () => {
   return (
-    <footer className=" bg-[#c9c6c6] text-black p-[3vh] font-inter font-sans">
-      <div className="container flex justify-between ">
-        <div className="flex justify-between ">
+    <footer className=" bg-[#d8d1d1] text-black p-[3vh] font-inter font-sans">
+      <div className=" flex justify-between px-[5vw] ">
 
           {/* Logo and description */}
           <div className='flex items-center'>
@@ -22,8 +21,8 @@ const Footer = () => {
               />
             </Link>
             {/* Discription */}
-            <div className='ml-4'>
-              <h1 className="text-2xl mb-2">Cognizen Club NITR</h1>
+            <div className='ml-4 font-normal'>
+              <h1 className="text-2xl font-semibold mb-2">Cognizen Club NITR</h1>
               <p>The official politics and economics</p>
               <p>awareness club of NIT Rourkela.</p>
               <p>Contact us: <span className='font-semibold cursor-pointer text-teal-950'>cognizenclub.nitr@gmail.com</span></p>
@@ -32,8 +31,8 @@ const Footer = () => {
           </div>
 
           {/* Social Media Links */}
-          <div>
-            <h3 className="text-lg font-semibold mb-4">Follow Us</h3>
+          <div className='flex flex-col  px-[5vw] py-3'>
+            <h3 className="text-2xl font-semibold mb-4">Follow Us</h3>
             <div className="flex space-x-4">
               <a href="https://www.facebook.com" className="text-[#FFFFFF] hover:text-orange-500">
                 <i className="fab fa-facebook-f"></i>
@@ -54,12 +53,12 @@ const Footer = () => {
           </div>
 
 
-        </div>
+        
 
       </div>
-        <div className="mt-8  border-gray-700 pt-4 text-center">
-          <p className="text-sm">&copy; 2024 Cognizen Club NITR. All rights reserved.</p>
-        </div>
+      <div className="mt-8  border-gray-700 pt-4 text-center">
+        <p className="text-sm">&copy; 2024 Cognizen Club NITR. All rights reserved.</p>
+      </div>
     </footer>
   );
 };
