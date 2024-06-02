6a0a0147a19280426d7359fd7f3ec14635605fd4
ZeeshanDEVPRO <professionalid2003@gmail.com>
2024-06-02 14:09:54 +0530

    cors fixed

--- frontend/src/adminComponents/AdminLeadership.jsx
+++ frontend/src/adminComponents/AdminLeadership.jsx
@@ -80,7 +80,7 @@ const handleImageChange = (e) => {
     formDataToSend.append('photo', formData.photo);
 
     try {
-      const response = await fetch('https://cognizen-backend-pearl.vercel.app/upload', {
+      const response = await fetch('https://cognizen-website.onrender.com/upload', {
         method: 'POST',
         body: formDataToSend,
         headers: {
@@ -163,7 +163,7 @@ const handleImageChange = (e) => {
     }
 
     try {
-      const response = await fetch(`https://cognizen-backend-pearl.vercel.app/delete/${deleteID}`, {
+      const response = await fetch(`https://cognizen-website.onrender.com/delete/${deleteID}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json'
